7a4ab58dfd9a5aa118661524bea762eb08ec74ae
Paweł Zieliński <pawelz72@gmail.com>
2020-12-14 22:23:31 +0100

    Add method insertRow, change method getArrayFromFile

---
+++ src/main/java/pl/indexpz/taskmanager/InsertRow.java
@@ -0,0 +1,31 @@
+package pl.indexpz.taskmanager;
+
+public class InsertRow {
+    public static void main(String[] args) {
+
+        String[][] test = {{"a1", "a2"}, {"b1", "b2"}};
+        String[] insert = {"c1", "c2"};
+        int row = 2;
+        test = insertRow(test, row, insert);
+        for (int i = 0; i < test.length; i++) {
+            for (int j = 0; j < test[i].length; j++) {
+                System.out.print(test[i][j] +" ");
+
+            }
+            System.out.println();
+
+        }
+
+    }
+    private static String[][] insertRow(String[][] listArray, int row, String[] data) {
+        String[][] outArray = new String[listArray.length + 1][];
+        for (int i = 0; i < row; i++) {
+            outArray[i] = listArray[i];
+        }
+        outArray[row] = data;
+        for (int i = row + 1; i < outArray.length; i++) {
+            outArray[i] = listArray[i - 1];
+        }
+        return outArray;
+    }
+}
--- src/main/java/pl/indexpz/taskmanager/TaskManager.java
+++ src/main/java/pl/indexpz/taskmanager/TaskManager.java
@@ -3,7 +3,6 @@ package pl.indexpz.taskmanager;
 import org.apache.commons.lang3.ArrayUtils;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,7 +15,7 @@ public class TaskManager {
 
     private static String[] OPTIONS = {"add", "remove", "list", "exit"};
     private static String[] INFO_STR = {"Please select an option:"};
-    private static String DATA_BASE_FILE = "src/main/java/pl/indexpz/taskmanager/tasks.csv";
+    private static String DATABASE_FILE = "src/main/java/pl/indexpz/taskmanager/tasks.csv";
 
 
     public static void main(String[] args) {
@@ -30,20 +29,17 @@ public class TaskManager {
 
     //  Czytam z pliku i tworzę tablicę/ Read from file and create array
     private static String[][] getArrayFromFile(String fileLocalisation) {
-        String[][] listArray = new String[0][3];
+        String[][] listArray = new String[1][3];
         Path path = Paths.get(fileLocalisation);
-        String[] array = new String[1];
-        int countRow = 0;
-        String str = "";
+        String[] array = new String[3];
+        int countRow = 1;
         if (Files.exists(path)) {
             try {
                 for (String line : Files.readAllLines(path)) {
+                    array = line.split(", ");
+                    listArray = insertRow(listArray, countRow, array);
                     countRow++;
-                    str += line + "\n";
-                    array = line.split(",");
-
                 }
-
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -51,36 +47,50 @@ public class TaskManager {
             System.out.println("File not exist");
         }
 
-        listArray = Arrays.copyOf(listArray, countRow);
-        System.out.println("Liczba wierszy " + listArray.length);
+//        Wyświetla listę / Print list
         for (int i = 0; i < listArray.length; i++) {
             for (int j = 0; j < listArray[i].length; j++) {
-                listArray[i][j] = array[i];
+                System.out.print(i + ": " + listArray[i][j] + " ");
             }
-            System.out.println(Arrays.toString(listArray));
+            System.out.println();
         }
-
-
         return listArray;
     }
+
+    // Dodawanie wiersza do tablicy dwuwymiarowej / Adding new row to array two dimensional
+    private static String[][] insertRow(String[][] listArray, int row, String[] data) {
+        String[][] outArray = new String[listArray.length + 1][];
+        for (int i = 0; i < row; i++) {
+            outArray[i] = listArray[i];
+        }
+        outArray[row] = data;
+        for (int i = row + 1; i < outArray.length; i++) {
+            outArray[i] = listArray[i - 1];
+        }
+        return outArray;
+    }
+
 
     //    Główna pętla z wyborem polecenia / Main loop with choisen option
     private static void mainLoopOption(String choiceOption) {
         if (choiceOption.equals(OPTIONS[0])) {
             System.out.println("Wykonuję add");
             showOptions();
+            mainLoopOption(choiceOption());
         } else if (choiceOption.equals(OPTIONS[1])) {
             System.out.println("Wykonuję remove");
             showOptions();
+            mainLoopOption(choiceOption());
         } else if (choiceOption.equals(OPTIONS[2])) {
-            System.out.println("Wykonuję list");
-            getArrayFromFile(DATA_BASE_FILE);
+            getArrayFromFile(DATABASE_FILE);
             showOptions();
+            mainLoopOption(choiceOption());
         } else if (choiceOption.equals(OPTIONS[3])) {
             System.out.println("Kończę program");
-            showOptions();
         } else {
             System.out.println("Wrong method. Try again:");
+            mainLoopOption(choiceOption());
+
         }
 
     }
