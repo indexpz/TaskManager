4dbb7309dd296a42cccd97781f2b09155bc75cc9
Paweł Zieliński <pawelz72@gmail.com>
2020-12-14 11:01:32 +0100

    Add Class TaskManager

---
+++ src/main/java/pl/indexpz/taskmanager/TaskManager.java
@@ -0,0 +1,13 @@
+package pl.indexpz.taskmanager;
+
+public class TaskManager {
+    public static void main(String[] args) {
+        taskManager();
+    }
+    public static void taskManager(){
+
+    }
+
+
+
+}
